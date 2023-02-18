6eee4dd06cb70fc806a88ea6ccf90eef378df3ad
mcnmtt <mattiamaucioni@icloud.com>
2023-02-18 18:24:09 +0100

    Modifica operazioni #2.

--- progettodb/src/OperationsDB.java
+++ progettodb/src/OperationsDB.java
@@ -2,185 +2,13 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class OperationsDB {
-/*
-    public ArrayList<Categoria> doRetrieveAllCategoria() {
 
-        ResultSet resultSet;
-
-        ArrayList<Categoria> lista = new ArrayList<>();
-
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ricette", "root", "admin")) {
-
-            PreparedStatement ps = con.prepareStatement("SELECT * FROM Categoria;");
-            resultSet = ps.executeQuery();
-
-            while (resultSet.next()) {
-
-                Categoria categoria = new Categoria();
-                categoria.setIdCategoria(resultSet.getInt(1));
-                categoria.setNome(resultSet.getString(2));
-
-                lista.add(categoria);
-            }
-            return lista;
-
-        } catch (SQLException e) {
-            throw new RuntimeException();
-        }
-    }
-
-    public ArrayList<Indirizzo> doRetrieveAllVia() {
-
-        ResultSet resultSet;
-
-        ArrayList<Indirizzo> lista = new ArrayList<>();
-
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ricette", "root", "admin")) {
-
-            PreparedStatement ps = con.prepareStatement("SELECT * FROM Via;");
-            resultSet = ps.executeQuery();
-
-            while (resultSet.next()) {
-
-                Indirizzo via = new Indirizzo();
-                via.setIdVia(resultSet.getInt(1));
-                via.setNome(resultSet.getString(2));
-                via.setCivico(resultSet.getInt(3));
-
-                lista.add(via);
-            }
-            return lista;
-
-        } catch (SQLException e) {
-            throw new RuntimeException();
-        }
-    }
-
-    public ArrayList<Utente> doRetrieveAllUtente() {
-
-        ResultSet resultSet;
-
-        ArrayList<Utente> lista = new ArrayList<>();
-
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ricette", "root", "admin")) {
-
-            PreparedStatement ps = con.prepareStatement("SELECT * FROM Utente;");
-            resultSet = ps.executeQuery();
-
-            while (resultSet.next()) {
-
-                Utente utente = new Utente();
-                utente.setEmail(resultSet.getString(1));
-                utente.setPsw(resultSet.getString(2));
-                utente.setNome(resultSet.getString(3));
-                utente.setCognome(resultSet.getString(4));
-                utente.setTelefono(resultSet.getString(5));
-                utente.setVia(resultSet.getString(6));
-                utente.setDataNascita(resultSet.getString(7));
-                utente.setFoto(resultSet.getString(8));
-
-                lista.add(utente);
-            }
-            return lista;
-
-        } catch (SQLException e) {
-            throw new RuntimeException();
-        }
-    }
-
-    public ArrayList<Ricetta> doRetrieveAllRicetta() {
-
-        ResultSet resultSet;
-
-        ArrayList<Ricetta> lista = new ArrayList<>();
-
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ricette", "root", "admin")) {
-
-            PreparedStatement ps = con.prepareStatement("SELECT * FROM Ricetta;");
-            resultSet = ps.executeQuery();
-
-            while (resultSet.next()) {
-
-                Ricetta ricetta = new Ricetta();
-                ricetta.setIdRicetta(resultSet.getInt(1));
-                ricetta.setNome(resultSet.getString(2));
-                ricetta.setFoto(resultSet.getString(3));
-                ricetta.setProcedimento(resultSet.getString(4));
-                ricetta.setTempoCottura(resultSet.getInt(5));
-                ricetta.setTempoPreparazione(resultSet.getInt(6));
-                ricetta.setKcal(resultSet.getInt(7));
-                ricetta.setEmail(resultSet.getString(8));
-                ricetta.setIdCategoria((resultSet.getInt(9)));
-
-                lista.add(ricetta);
-            }
-            return lista;
-
-        } catch (SQLException e) {
-            throw new RuntimeException();
-        }
-    }
-
-    public ArrayList<Preferito> doRetrieveAllPreferito() {
-
-        ResultSet resultSet;
-
-        ArrayList<Preferito> lista = new ArrayList<>();
-
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ricette", "root", "admin")) {
-
-            PreparedStatement ps = con.prepareStatement("SELECT * FROM Preferito;");
-            resultSet = ps.executeQuery();
-
-            while (resultSet.next()) {
-
-                Preferito preferito = new Preferito();
-                preferito.setIdRicetta(resultSet.getInt(1));
-                preferito.setEmail(resultSet.getString(2));
-
-                lista.add(preferito);
-            }
-            return lista;
-
-        } catch (SQLException e) {
-            throw new RuntimeException();
-        }
-    }
-
-    public ArrayList<Recensione> doRetrieveAllRecensione() {
-
-        ResultSet resultSet;
-
-        ArrayList<Recensione> lista = new ArrayList<>();
-
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ricette", "root", "admin")) {
-
-            PreparedStatement ps = con.prepareStatement("SELECT * FROM Recensione;");
-            resultSet = ps.executeQuery();
-
-            while (resultSet.next()) {
-
-                Recensione recensione = new Recensione();
-                recensione.setIdRecensione(resultSet.getInt(1));
-                recensione.setTesto(resultSet.getString(2));
-                recensione.setIdTipologiaRecensione(resultSet.getInt(3));
-
-                lista.add(recensione);
-            }
-            return lista;
-
-        } catch (SQLException e) {
-            throw new RuntimeException();
-        }
-    }
-
-*/
     //OPERAZIONE #1
     public void operazioneUno(Utente utente) {
 
         try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ricette", "root", "admin")) {
             PreparedStatement ps = con.prepareStatement(
-                    "INSERT INTO Utente (" +
+                    "INSERT INTO utente (" +
                             "email, " +
                             "psw, " +
                             "nome, " +
@@ -214,13 +42,13 @@ public class OperationsDB {
             throw new RuntimeException(e);
         }
     }
-/*
+
     //OPERAZIONE #2
     public void operazioneDue(Ricetta ricetta) {
 
         try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ricette", "root", "admin")) {
             PreparedStatement ps = con.prepareStatement(
-                    "INSERT INTO Ricetta (" +
+                    "INSERT INTO ricetta (" +
                             "idRicetta, " +
                             "nome, " +
                             "foto, " +
@@ -250,164 +78,12 @@ public class OperationsDB {
         }
     }
 
-    //OPERAZIONE 3
-    public void operazioneTre(Recensione recensione, Scrive scrive) {
-
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ricette", "root", "admin")) {
-            PreparedStatement ps = con.prepareStatement(
-                    "INSERT INTO Recensione (" +
-                            "idRecensione, " +
-                            "testo, " +
-                            "idTipologiaRecensione" +
-                            ") VALUES(?,?,?)");
-
-            ps.setInt(1, recensione.getIdRecensione());
-            ps.setString(2, recensione.getTesto());
-            ps.setInt(3, recensione.getIdTipologiaRecensione());
-
-            if (ps.executeUpdate() != 1) {
-                throw new RuntimeException("INSERT error.");
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ricette", "root", "admin")) {
-            PreparedStatement ps = con.prepareStatement(
-                    "INSERT INTO Scrive (" +
-                            "email, " +
-                            "idRecensione, " +
-                            "idRicetta" +
-                            ") VALUES(?,?,?)");
-
-            ps.setString(1, scrive.getEmail());
-            ps.setInt(2, scrive.getIdRecensione());
-            ps.setInt(3, scrive.getIdRicetta());
-
-            if (ps.executeUpdate() != 1) {
-                throw new RuntimeException("INSERT error.");
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    //OPERAZIONE 4
-    public ArrayList<Ricetta> operazioneQuattro(String email) {
-
-        ArrayList<Ricetta> listaRicette = new ArrayList<>();
-
-        ResultSet resultSet;
-
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ricette", "root", "admin")) {
-
-            PreparedStatement ps = con.prepareStatement("SELECT * FROM ricetta WHERE email=?");
-
-            ps.setString(1, email);
-
-            resultSet = ps.executeQuery();
-
-            while (resultSet.next()) {
-
-                Ricetta ricetta = new Ricetta();
-
-                ricetta.setIdRicetta(resultSet.getInt(1));
-                ricetta.setNome(resultSet.getString(2));
-                ricetta.setFoto(resultSet.getString(3));
-                ricetta.setProcedimento(resultSet.getString(4));
-                ricetta.setTempoCottura(resultSet.getInt(5));
-                ricetta.setTempoPreparazione(resultSet.getInt(6));
-                ricetta.setKcal(resultSet.getInt(7));
-                ricetta.setEmail(email);
-                ricetta.setIdCategoria(resultSet.getInt(9));
-
-                listaRicette.add(ricetta);
-            }
-            return listaRicette;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    //OPERAZIONE 5
-    public ArrayList<Ricetta> operazioneCinque(int idCategoria) {
-
-        ArrayList<Ricetta> listaRicette = new ArrayList<>();
-
-        ResultSet resultSet;
-
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ricette", "root", "admin")) {
-
-            PreparedStatement ps = con.prepareStatement("SELECT * FROM ricetta WHERE idCategoria=?");
-
-            ps.setInt(1, idCategoria);
-
-            resultSet = ps.executeQuery();
-
-            while (resultSet.next()) {
-
-                Ricetta ricetta = new Ricetta();
-
-                ricetta.setIdRicetta(resultSet.getInt(1));
-                ricetta.setNome(resultSet.getString(2));
-                ricetta.setFoto(resultSet.getString(3));
-                ricetta.setProcedimento(resultSet.getString(4));
-                ricetta.setTempoCottura(resultSet.getInt(5));
-                ricetta.setTempoPreparazione(resultSet.getInt(6));
-                ricetta.setKcal(resultSet.getInt(7));
-                ricetta.setEmail(resultSet.getString(8));
-                ricetta.setIdCategoria(idCategoria);
-
-                listaRicette.add(ricetta);
-            }
-            return listaRicette;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    //OPERAZIONE 6
-    public ArrayList<Ricetta> operazioneSei() {
-
-        ArrayList<Ricetta> listaRicette = new ArrayList<>();
-
-        ResultSet resultSet;
-
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ricette", "root", "admin")) {
-
-            PreparedStatement ps = con.prepareStatement("SELECT *" +
-                    "FROM Ricetta INNER JOIN Preferito ON Ricetta.idRicetta = Preferito.idRicetta");
-
-            resultSet = ps.executeQuery();
-
-            while (resultSet.next()) {
-
-                Ricetta ricetta = new Ricetta();
-
-                ricetta.setIdRicetta(resultSet.getInt(1));
-                ricetta.setNome(resultSet.getString(2));
-                ricetta.setFoto(resultSet.getString(3));
-                ricetta.setProcedimento(resultSet.getString(4));
-                ricetta.setTempoCottura(resultSet.getInt(5));
-                ricetta.setTempoPreparazione(resultSet.getInt(6));
-                ricetta.setKcal(resultSet.getInt(7));
-                ricetta.setEmail(resultSet.getString(8));
-                ricetta.setIdCategoria(resultSet.getInt(9));
-
-                listaRicette.add(ricetta);
-            }
-            return listaRicette;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    //OPERAZIONE 7
+    //OPERAZIONE #3
     public void deleteRicetta(int idRicetta) {
 
         try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ricette", "root", "admin")) {
 
-            PreparedStatement ps = con.prepareStatement("DELETE FROM ricetta WHERE idRicetta=?");
+            PreparedStatement ps = con.prepareStatement("DELETE FROM ricetta WHERE idRicetta = ?");
 
             ps.setInt(1, idRicetta);
 
@@ -419,8 +95,8 @@ public class OperationsDB {
         }
     }
 
-    //OPERAZIONE 8
-    public ArrayList<Ricetta> operazioneOtto(String email) {
+    //OPERAZIONE #4
+    public ArrayList<Ricetta> operazioneQuattro(String email) {
 
         ArrayList<Ricetta> listaRicette = new ArrayList<>();
 
@@ -428,13 +104,9 @@ public class OperationsDB {
 
         try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ricette", "root", "admin")) {
 
-            PreparedStatement ps = con.prepareStatement("SELECT *" +
-                    "FROM Ricetta" +
-                    "WHERE Preferito.email = ? AND Ricetta.email = ?" +
-                    "ORDER BY SUM(Ricetta.tempoPreparazione * Ricetta.tempoCottura) ASC;");
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM ricetta WHERE email = ?");
 
             ps.setString(1, email);
-            ps.setString(2, email);
 
             resultSet = ps.executeQuery();
 
@@ -460,82 +132,4 @@ public class OperationsDB {
         }
     }
 
-    //OPERAZIONE 9
-    public int operazioneNove(String email) {
-
-        int i = 0;
-
-        ResultSet resultSet;
-
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ricette", "root", "admin")) {
-
-            PreparedStatement ps = con.prepareStatement("" +
-                    "SELECT numRicettePubblicate" +
-                    "FROM Utente" +
-                    "WHERE email LIKE ?;");
-
-            ps.setString(1, email);
-
-            resultSet = ps.executeQuery();
-
-            while (resultSet.next()) {
-                i = resultSet.getInt(1);
-            }
-            return i;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    //OPERAZIONE 10
-    public ArrayList<String> operazioneDieci() {
-
-        ResultSet resultSet;
-
-        ArrayList<String> lista = new ArrayList<>();
-
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ricette", "root", "admin")) {
-
-            PreparedStatement ps = con.prepareStatement("SELECT email" +
-                    "FROM Utente" +
-                    "WHERE Utente.telefono IN (\"+39%\",\"+33%\",\"+49%\")" +
-                    "ORDER BY email ASC;");
-
-            resultSet = ps.executeQuery();
-
-            while (resultSet.next()) {
-
-                String email;
-                email = resultSet.getString(1);
-
-                lista.add(email);
-            }
-            return lista;
-
-        } catch (SQLException e) {
-            throw new RuntimeException();
-        }
-    }
-
-    //PRENDERE ULTIMO INDICE RECENSIONI
-    public int retrieveNumberOfRecensioni() {
-
-        int num = 0;
-
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ricette", "root", "admin")) {
-
-            PreparedStatement ps = con.prepareStatement("SELECT COUNT(*) FROM recensione");
-
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-
-                num = rs.getInt(1);
-
-            }
-            return num;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    */
 }
--- progettodb/src/PortaleRicette.java
+++ progettodb/src/PortaleRicette.java
@@ -8,16 +8,8 @@ public class PortaleRicette {
         System.out.println("Benvenuto nel portale di ricette, scegli l'operazione che vuoi svolgere:" +
                 "\n1. Aggiungi nuovo utente." +
                 "\n2. Aggiungi nuova ricetta." +
-                "\n3. Aggiungi recensione ad una ricetta (con o senza voto)." +
-                "\n4. Selezionare tutte le ricette di un utente." +
-                "\n5. Selezionare tutte le ricette di una categoria." +
-                "\n6. Selezionare le ricette presenti in tutti i preferiti." +
-                "\n7. Rimuovere una ricetta." +
-                "\n8. Selezionare lista ricette ordinate per tempo totale di preparazione (preparazione+cottura)" +
-                "crescente nella lista dei preferiti di un certo utente." +
-                "\n9. Selezionare il numero di ricette inserite da un utente." +
-                "\n10. Selezionare una lista di email ordinate in ordine alfabetico corrispondenti agli utenti" +
-                "aventi numero di telefono con prefisso Italiano, Francese o Tedesco" +
+                "\n3. Rimuovere una ricetta." +
+                "\n4. Selezionare il numero di ricette inserite da un utente." +
                 "\n\n Seleziona la tua scelta e dai invio.");
 
         Scanner sc = new Scanner(System.in);
@@ -57,7 +49,7 @@ public class PortaleRicette {
 
                 System.out.println("Operazione eseguita con successo.\n");
 
-                break;/*
+                break;
             case 2:
                 System.out.println("Hai scelto 'Aggiungi nuova ricetta.'");
                 Ricetta ricetta = new Ricetta();
@@ -85,58 +77,6 @@ public class PortaleRicette {
 
                 break;
             case 3:
-                System.out.println("Hai scelto 'Aggiungi recensione ad una ricetta (con o senza voto).'");
-                Recensione recensione = new Recensione();
-                Scrive scrive = new Scrive();
-
-                System.out.println("Scegli tipologia recensione (1. NO RATING to 6. 5 STELLE): ");
-                recensione.setIdTipologiaRecensione(sc.nextInt());
-                int ID = (service.retrieveNumberOfRecensioni());
-                recensione.setIdRecensione(ID+1);
-                System.out.println("Testo:");
-                recensione.setTesto(sc.next());
-                System.out.println("Email:");
-                scrive.setEmail(sc.next());
-                System.out.println("ID Ricetta:");
-                scrive.setIdRicetta(sc.nextInt());
-                scrive.setIdRecensione(ID+1);
-
-                service.operazioneTre(recensione, scrive);
-
-                System.out.println("Operazione eseguita con successo.\n");
-
-                break;
-            case 4:
-                System.out.println("Hai scelto 'Selezionare tutte le ricette di un utente.'");
-
-                System.out.println("Email:");
-                ArrayList<Ricetta> ricette;
-                ricette = service.operazioneQuattro(sc.next());
-
-                System.out.println("Operazione eseguita con successo.");
-                System.out.println(ricette);
-
-                break;
-            case 5:
-                System.out.println("Hai scelto 'Selezionare tutte le ricette di una categoria.'");
-
-                System.out.println("IDCategoria:");
-                ricette = service.operazioneCinque(sc.nextInt());
-
-                System.out.println("Operazione eseguita con successo.");
-                System.out.println(ricette);
-
-                break;
-            case 6:
-                System.out.println("Hai scelto 'Selezionare le ricette presenti in tutti i preferiti.'");
-
-                ricette = service.operazioneSei();
-
-                System.out.println("Operazione eseguita con successo.");
-                System.out.println(ricette);
-
-                break;
-            case 7:
                 System.out.println("Hai scelto 'Rimuovere una ricetta.'");
 
                 System.out.println("ID Ricetta:");
@@ -145,35 +85,17 @@ public class PortaleRicette {
                 System.out.println("Operazione eseguita con successo.");
 
                 break;
-            case 8:
-                System.out.println("Hai scelto 'Selezionare lista ricette ordinate per tempo totale di preparazione (preparazione+cottura)'");
-
-                System.out.println("Email:");
-                ricette = service.operazioneOtto(sc.next());
-
-                System.out.println("Operazione eseguita con successo.");
-                System.out.println(ricette);
-
-                break;
-            case 9:
+            case 4:
                 System.out.println("Hai scelto 'Selezionare il numero di ricette inserite da un utente.'");
 
+                ArrayList<Ricetta> ricette;
                 System.out.println("Email:");
-                ricette = service.operazioneOtto(sc.next());
+                ricette = service.operazioneQuattro(sc.next());
 
                 System.out.println("Operazione eseguita con successo.");
                 System.out.println(ricette.size());
 
                 break;
-            case 10:
-                System.out.println("Hai scelto 'Selezionare una lista di email ordinate in ordine alfabetico corrispondenti agli utenti aventi numero di telefono con prefisso Italiano, Francese o Tedesco'");
-
-                ArrayList<String> listEmail = service.operazioneDieci();
-
-                System.out.println("Operazione eseguita con successo.");
-                System.out.println(listEmail);
-
-                break;*/
             default:
                 System.out.println("Errore. Scelta non valida!");
         }
